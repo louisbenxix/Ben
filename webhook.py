7988eaf0d3264e7c2078d35af9b75598a4df8b8a
louisbenxix <huynh.tri.012@gmail.com>
2024-12-06 13:17:35 +0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Thay token và chat_id của bạn vào đây
 BOT_TOKEN = "7726901239:AAEfAG8kzDU8aG7W99vJvV5D_7zyX7V7bXQ"  # Đặt trong dấu ngoặc kép
-CHAT_ID = "968290827"  # Đặt trong dấu ngoặc kép
+CHAT_ID = "1213184614"  # Đặt trong dấu ngoặc kép
 
 # Hàm gửi tin nhắn đến Telegram
 def send_telegram_message(message):
